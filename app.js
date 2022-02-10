bb530945982a41fa0d25e5f656c5be7fd24e923b
m-ghiringhelli <mghiring@gmail.com>
2022-02-09 17:43:52 -0800

    renders presidents as elements on homepage

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import { getPresidents } from './fetch-utils.js';
+import { renderPresidents } from './render-utils.js';
+
+const presContainer = document.getElementById('presidents-container');
 
 window.addEventListener('load', async() => {
     const presidents = await getPresidents();
     console.log(presidents, 'presidents');
+    for (let president of presidents) {
+        const presEl = renderPresidents(president);
+        presContainer.append(presEl);
+    }
 });
