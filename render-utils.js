dfe1c11da84b3f60428337b7374d9be6ea3c6083
m-ghiringhelli <mghiring@gmail.com>
2022-02-11 11:20:58 -0800

    render nav bar function

--- render-utils.js
+++ render-utils.js
@@ -37,3 +37,28 @@ export function renderDetail(president) {
     div.append(h1, img, p1, p2, p3, p4, p5);
     return div;
 }
+
+export function renderNav(id) {
+    const div = document.createElement('div');
+    div.setAttribute('id', 'nav');
+    const a1 = document.createElement('a');
+    a1.href = `./?id=${id - 1}`;
+    const span1 = document.createElement('span');
+    span1.setAttribute('id', 'nav1');
+    span1.textContent = 'previous president';
+    const a2 = document.createElement('a');
+    a2.href = `../index.html`;
+    const span2 = document.createElement('span');
+    span2.setAttribute('id', 'nav2');
+    span2.textContent = 'back to the list';
+    const a3 = document.createElement('a');
+    a3.href = `./?id=${id + 1}`;
+    const span3 = document.createElement('span');
+    span3.setAttribute('id', 'nav3');
+    span3.textContent = 'next president';
+    a1.append(span1);
+    a2.append(span2);
+    a3.append(span3);
+    div.append(a1, a2, a3);
+    return div;
+}
--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,4 @@
-import { renderPresidents, renderDetail } from '../render-utils.js';
+import { renderPresidents, renderDetail, renderNav } from '../render-utils.js';
 
 const test = QUnit.test;
 
@@ -12,6 +12,17 @@ const president = {
     'birthplace': 'Point Pleasant, Ohio',
     'image': 'grant.jpg'
 };
+
+const id = 3;
+
+test('renderNav should create three html elements for nav', (expect) => {
+    const expected = `<div id="nav"><a href="./?id=2"><span id="nav1">previous president</span></a><a href="../index.html"><span id="nav2">back to the list</span></a><a href="./?id=4"><span id="nav3">next president</span></a></div>`;
+
+    const actual = renderNav(id);
+
+    expect.equal(actual.outerHTML, expected);
+});
+
 test('renderDetail should return details element', (expect) => {
     const expected = `<div id="president-container"><h1>ULYSSES S. GRANT</h1><img src="../assets/grant.jpg"><p class="term">Years in office:</p><p class="term">1869-1877</p><p class="born">Birthplace:</p><p class="born">Point Pleasant, Ohio</p><p id="funfact">While almost penniless and dying of throat cancer from smoking so many cigars, he wrote his memoirs to try and raise some money. He died only a few days after finishing. They are regarded as not only some of the best presidential memoirs, but are also held in high literary regard for the quality of the writing.</p></div>`;
 
